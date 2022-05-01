d8400a95ca4f6242c44048cc2c08c47efeebbb76
FredFed <thenedbear@hotmail.it>
2022-05-01 15:44:58 +0200

    created 403 document

---
+++ forbidden.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Imaged | Forbidden</title>
+    </head>
+
+    <body>
+        <header>
+        </header>
+
+        <section id="page_main_section">
+
+            <!-- MAIN SECTION -->
+            <div class="missing-frame">
+                <h1 class="missing">403<br></h1>
+                <h2 class="missing-desc">Forbidden: you don't have the necessary permissions to visualize the requested resource or visit the requested folder.</h2>
+                <a class="missing-home-button button" href="<?php echo $_SERVER["SERVER_NAME"]; ?>">Homepage</a>
+            </div>
+
+        </section>
+
+        <footer>
+        </footer>
+
+    </body>
+</html>
